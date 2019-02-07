1cd6b100e905b81cc4f22e765f71a59c8fbd225b
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-06 19:10:00 -0600

    Update main.cpp
    Addition

--- main.cpp
+++ main.cpp
@@ -5,13 +5,23 @@ int main(){
 	
 	char op = 'A';
 	char again = 'Y';
+	int num1=0;
+	int num2=0;
 	
 	
 	cout << "Our C++ Calculator. \n";
 	
 	while ('n' != again){
-		cout << " (A)dd or (S)ubtract? ";
+		cout << "(A)dd or (S)ubtract? ";
 		cin >> op;
+		
+		cout << "Enter the first number: ";
+		cin >> num1;
+		cout << "Enter the second number: ";
+		cin >> num2;
+		
+		cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+		
 		cout << "Do you want to use Calculator again (Y/n)? ";
 		cin >> again;
 	}
